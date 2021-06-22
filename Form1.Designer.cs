fb77fff8f70eb0d170a475200c29083996b5ef04
L00086023 <L00086023@student.lyit.ie>
2021-06-22 15:28:52 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Dates_and_Strings_exercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblArrivalDate = new System.Windows.Forms.Label();
+            this.lblDepartureDate = new System.Windows.Forms.Label();
+            this.lblNumberOfNights = new System.Windows.Forms.Label();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            this.lblAvgPricePerNight = new System.Windows.Forms.Label();
+            this.txtBxNoOfNights = new System.Windows.Forms.TextBox();
+            this.txtBxTotalPrice = new System.Windows.Forms.TextBox();
+            this.txtBxAvgPricePerNight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dtPikrArrivalDate = new System.Windows.Forms.DateTimePicker();
+            this.dtPkrDepartureDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lblArrivalDate
+            // 
+            this.lblArrivalDate.AutoSize = true;
+            this.lblArrivalDate.Location = new System.Drawing.Point(180, 66);
+            this.lblArrivalDate.Name = "lblArrivalDate";
+            this.lblArrivalDate.Size = new System.Drawing.Size(70, 14);
+            this.lblArrivalDate.TabIndex = 0;
+            this.lblArrivalDate.Text = "Arrival date:";
+            // 
+            // lblDepartureDate
+            // 
+            this.lblDepartureDate.AutoSize = true;
+            this.lblDepartureDate.Location = new System.Drawing.Point(180, 115);
+            this.lblDepartureDate.Name = "lblDepartureDate";
+            this.lblDepartureDate.Size = new System.Drawing.Size(93, 14);
+            this.lblDepartureDate.TabIndex = 1;
+            this.lblDepartureDate.Text = "Departure  date:";
+            // 
+            // lblNumberOfNights
+            // 
+            this.lblNumberOfNights.AutoSize = true;
+            this.lblNumberOfNights.Location = new System.Drawing.Point(180, 162);
+            this.lblNumberOfNights.Name = "lblNumberOfNights";
+            this.lblNumberOfNights.Size = new System.Drawing.Size(100, 14);
+            this.lblNumberOfNights.TabIndex = 2;
+            this.lblNumberOfNights.Text = "Number of nights:";
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.Location = new System.Drawing.Point(180, 211);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(63, 14);
+            this.lblTotalPrice.TabIndex = 3;
+            this.lblTotalPrice.Text = "Total price";
+            // 
+            // lblAvgPricePerNight
+            // 
+            this.lblAvgPricePerNight.AutoSize = true;
+            this.lblAvgPricePerNight.Location = new System.Drawing.Point(180, 269);
+            this.lblAvgPricePerNight.Name = "lblAvgPricePerNight";
+            this.lblAvgPricePerNight.Size = new System.Drawing.Size(109, 14);
+            this.lblAvgPricePerNight.TabIndex = 4;
+            this.lblAvgPricePerNight.Text = "Avg. price per night";
+            // 
+            // txtBxNoOfNights
+            // 
+            this.txtBxNoOfNights.Location = new System.Drawing.Point(383, 162);
+            this.txtBxNoOfNights.Name = "txtBxNoOfNights";
+            this.txtBxNoOfNights.Size = new System.Drawing.Size(97, 22);
+            this.txtBxNoOfNights.TabIndex = 7;
+            // 
+            // txtBxTotalPrice
+            // 
+            this.txtBxTotalPrice.Location = new System.Drawing.Point(383, 211);
+            this.txtBxTotalPrice.Name = "txtBxTotalPrice";
+            this.txtBxTotalPrice.Size = new System.Drawing.Size(97, 22);
+            this.txtBxTotalPrice.TabIndex = 8;
+            // 
+            // txtBxAvgPricePerNight
+            // 
+            this.txtBxAvgPricePerNight.Location = new System.Drawing.Point(380, 269);
+            this.txtBxAvgPricePerNight.Name = "txtBxAvgPricePerNight";
+            this.txtBxAvgPricePerNight.Size = new System.Drawing.Size(100, 22);
+            this.txtBxAvgPricePerNight.TabIndex = 9;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(171, 346);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(128, 41);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(439, 356);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(116, 31);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dtPikrArrivalDate
+            // 
+            this.dtPikrArrivalDate.Location = new System.Drawing.Point(383, 77);
+            this.dtPikrArrivalDate.Name = "dtPikrArrivalDate";
+            this.dtPikrArrivalDate.Size = new System.Drawing.Size(97, 22);
+            this.dtPikrArrivalDate.TabIndex = 12;
+            this.dtPikrArrivalDate.ValueChanged += new System.EventHandler(this.dtPikrArrivalDate_ValueChanged);
+            // 
+            // dtPkrDepartureDate
+            // 
+            this.dtPkrDepartureDate.Location = new System.Drawing.Point(388, 115);
+            this.dtPkrDepartureDate.Name = "dtPkrDepartureDate";
+            this.dtPkrDepartureDate.Size = new System.Drawing.Size(92, 22);
+            this.dtPkrDepartureDate.TabIndex = 13;
+            this.dtPkrDepartureDate.ValueChanged += new System.EventHandler(this.dtPkrDepartureDate_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtPkrDepartureDate);
+            this.Controls.Add(this.dtPikrArrivalDate);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtBxAvgPricePerNight);
+            this.Controls.Add(this.txtBxTotalPrice);
+            this.Controls.Add(this.txtBxNoOfNights);
+            this.Controls.Add(this.lblAvgPricePerNight);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.lblNumberOfNights);
+            this.Controls.Add(this.lblDepartureDate);
+            this.Controls.Add(this.lblArrivalDate);
+            this.Name = "Form1";
+            this.Text = "Reservations";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblArrivalDate;
+        private System.Windows.Forms.Label lblDepartureDate;
+        private System.Windows.Forms.Label lblNumberOfNights;
+        private System.Windows.Forms.Label lblTotalPrice;
+        private System.Windows.Forms.Label lblAvgPricePerNight;
+        private System.Windows.Forms.TextBox txtBxNoOfNights;
+        private System.Windows.Forms.TextBox txtBxTotalPrice;
+        private System.Windows.Forms.TextBox txtBxAvgPricePerNight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DateTimePicker dtPikrArrivalDate;
+        private System.Windows.Forms.DateTimePicker dtPkrDepartureDate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dates_and_Strings_exercise
+{
+    public partial class Form1 : Form
+    {
+        DateTime arrivalDate;
+        DateTime departureDate;
+        Decimal TotalPrice;
+        int NoOfNights;
+        double AvgPrice;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dtPikrArrivalDate_ValueChanged(object sender, EventArgs e)
+        {
+            arrivalDate = dtPikrArrivalDate.Value;
+
+            arrivalDate = arrivalDate.Date;
+
+        }
+        public int NumberOfNights(DateTime startDate, DateTime endDate)
+        {
+            NoOfNights = Convert.ToInt32((endDate - startDate).TotalDays);
+
+            return NoOfNights;
+        }
+
+        public Decimal TotalPriceOfStay(string DurationOfStay)
+
+        {
+            int Duration = Convert.ToInt32(DurationOfStay);
+            TotalPrice = Duration * 120.00M;
+            return TotalPrice;
+
+        }
+        public double AvgPricePerNight()
+           {
+            AvgPrice = Convert.ToDouble(TotalPrice / NoOfNights);
+            return AvgPrice;
+
+    }
+
+
+        private void dtPkrDepartureDate_ValueChanged(object sender, EventArgs e)
+        {
+            departureDate = dtPkrDepartureDate.Value;
+            departureDate = departureDate.Date;
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtBxNoOfNights.Text = Convert.ToString(NumberOfNights(arrivalDate, departureDate));
+            txtBxTotalPrice.Text = Convert.ToString(TotalPriceOfStay(Convert.ToString(NoOfNights)));
+            txtBxAvgPricePerNight.Text = Convert.ToString(AvgPrice);/*This is not working at the moment. The AvgPrice field is not being updated? */
+        }
+    }
+}
